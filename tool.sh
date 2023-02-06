6961fc361daaff1a5edf24063bc4c1267779798f
f1161291 <78488261+f1161291@users.noreply.github.com>
2023-02-06 23:40:47 +0800

    Update tool.sh

--- tool.sh
+++ tool.sh
@@ -81,19 +81,19 @@ root_user(){
 }
 
 open_ports(){
-  systemctl stop firewalld.service 2>/dev/null
-  systemctl disable firewalld.service 2>/dev/null
-  setenforce 0 2>/dev/null
-  ufw disable 2>/dev/null
-  iptables -P INPUT ACCEPT 2>/dev/null
-  iptables -P FORWARD ACCEPT 2>/dev/null
-  iptables -P OUTPUT ACCEPT 2>/dev/null
-  iptables -t nat -F 2>/dev/null
-  iptables -t mangle -F 2>/dev/null
-  iptables -F 2>/dev/null
-  iptables -X 2>/dev/null
-  netfilter-persistent save 2>/dev/null
-  green "VPS的防火墙端口已放行！"
+systemctl stop firewalld.service 2>/dev/null
+systemctl disable firewalld.service 2>/dev/null
+setenforce 0 2>/dev/null
+ufw disable 2>/dev/null
+iptables -P INPUT ACCEPT 2>/dev/null
+iptables -P FORWARD ACCEPT 2>/dev/null
+iptables -P OUTPUT ACCEPT 2>/dev/null
+iptables -t nat -F 2>/dev/null
+iptables -t mangle -F 2>/dev/null
+iptables -F 2>/dev/null
+iptables -X 2>/dev/null
+netfilter-persistent save 2>/dev/null
+green "VPS的防火墙端口已放行！"
 }
 
 install_xui(){
