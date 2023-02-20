e7a4076caba181fef2b1e29f4e58e5d5acc310ea
f1161291 <78488261+f1161291@users.noreply.github.com>
2023-02-21 02:11:59 +0800

    Update tool.sh

--- tool.sh
+++ tool.sh
@@ -552,8 +552,8 @@ menu(){
 		1) root_user ;;
 		2) open_ports ;;
 		3) acme_rg ;;
-		4) bash <(wget --no-check-certificate -qO- 'https://raw.githubusercontent.com/MoeClub/Note/master/InstallNET.sh') -d 10 -v 64 -p cc20130401 ;;
-		5) apt install docker.io -y ;;
+		4) bash <(wget --no-check-certificate -qO- 'https://raw.githubusercontent.com/MoeClub/Note/master/InstallNET.sh') -d 10 -v 64 -p 123456789 ;;
+		5) bash <(curl -sSL https://gitee.com/SuperManito/LinuxMirrors/raw/main/DockerInstallation.sh) ;;
 		6) curl -fsSL "https://alist.nn.ci/v3.sh" | bash -s install ;;
                 7) install_xui ;;
                 8) curl -s http://docker.xiaoya.pro/update_xiaoya.sh | bash ;;
