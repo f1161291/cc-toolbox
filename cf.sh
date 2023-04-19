d3bb46d98756e6bb2b7fe7f6d5ac83d51678a242
f1161291 <78488261+f1161291@users.noreply.github.com>
2023-04-20 06:28:49 +0800

    Create cf.sh

---
+++ cf.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# 设置默认参数值
+n=500
+tl=200
+tll=20
+tp=8443
+sl=1
+url="https://cf.xiray.cf/1bg.test"
+
+# 解析命令行参数
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -n)
+            n="$2"
+            shift 2
+            ;;
+        -tl)
+            tl="$2"
+            shift 2
+            ;;
+        -tll)
+            tll="$2"
+            shift 2
+            ;;
+        -tp)
+            tp="$2"
+            shift 2
+            ;;
+        -sl)
+            sl="$2"
+            shift 2
+            ;;
+        -url)
+            url="$2"
+            shift 2
+            ;;
+        *)
+            echo "未知的选项: $1"
+            exit 1
+            ;;
+    esac
+done
+
+# 运行 Cloudflare 命令
+./Cloudflare -n "$n" -tl "$tl" -tll "$tll" -tp "$tp" -sl "$sl" -url "$url"
